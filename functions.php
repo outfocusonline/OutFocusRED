3ec299604602613d616f08aef5c30bee380286e2
Muthu Kumar <muthukumar@thefeathers.in>
2016-12-13 16:14:39 +0530

    Readme

--- functions.php
+++ functions.php
@@ -161,3 +161,10 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+function _remove_script_version( $src ){ 
+$parts = explode( '?', $src ); 	
+return $parts[0]; 
+} 
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 ); 
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
